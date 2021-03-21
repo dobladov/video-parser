c464f450bbe8bf29311141719bbf14b559cf2d23
Daniel Doblado <danieldoblado@gmail.com>
2021-03-21 19:37:43 +0100

    support ident and version

--- README.md
+++ README.md
@@ -2,21 +2,29 @@
 
 CLI app/package to extract metadata from a movie or TV show name.
 
+### Arguments
+```
+-p, --parse      String with the name of the show/movie
+-i, --indent     Pretty print the object with the given numeric indent
+-v, --version    Current version
+```
+
 ## Usage
 
 ```bash
-video-parser -p "Dawn.Of.The.Planet.of.The.Apes.2014.1080p.WEB-DL.DD51.H264-RARBG
+video-parser -p "Dawn.Of.The.Planet.of.The.Apes.2014.1080p.WEB-DL.AAC5.1.H264-RARBG" -i 2
 ```
 
 Result
 ```json
 {
-   "name": "Dawn Of The Planet of The Apes",
-   "year": 2014,
-   "resolution": "1080p",
-   "codec": "h264",
-   "source": "web-dl",
-   "audio": "DD"
+  "name": "Dawn Of The Planet of The Apes",
+  "year": "2014",
+  "resolution": "1080p",
+  "codec": "h264",
+  "source": "web-dl",
+  "audio": "AAC5.1",
+  "group": "RARBG"
 }
 ```
 
--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,35 @@
+import 'dart:io';
 import 'dart:convert';
 import 'package:args/args.dart';
 import 'package:video_parser/parseName.dart';
+
+final packageVersion = '1.0.0';
 
 void main(List<String> arguments) {
   var parser = ArgParser();
 
   parser.addOption('parse',
-      abbr: 'p', help: 'String with the full name of the show or movie');
+      abbr: 'p', help: 'String with the name of the show/movie');
+  parser.addOption('indent',
+      abbr: 'i', help: 'Pretty print the object with the given numeric indent');
+  parser.addFlag('version',
+      abbr: 'v', help: 'Current version', negatable: false);
 
   var args = parser.parse(arguments);
   var name = args['parse'];
+  var indent = args['indent'];
+  var version = args['version'];
+
+  if (version) {
+    print('v$packageVersion');
+    exit(0);
+  }
 
   if (name != null) {
-    print(jsonEncode(parseName(name)));
+    var encoder = indent != null
+        ? JsonEncoder.withIndent(' ' * int.parse(indent))
+        : JsonEncoder();
+    print(encoder.convert(parseName(name)));
   } else {
     print(parser.usage);
   }
